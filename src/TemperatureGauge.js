f61d07f3dc90ec8f6776c95e14de3051eea584f0
Mark Soderquist <mark@soderquist.net>
2022-03-13 10:54:04 -0600

    Add feels like temperature

--- src/TemperatureGauge.js
+++ src/TemperatureGauge.js
@@ -5,9 +5,13 @@ export default class TemperatureGauge extends Component {
 
 	render() {
 		const temperature = parseFloat(this.props.weather.temperature).toFixed(1);
+		const feelslike = parseFloat(this.props.weather.feelsLike).toFixed(1);
 		const unit = this.props.weather.temperatureUnit;
 		return (
+			<div>
 			<div className="temperature">{temperature}<span className="unit">{unit}</span></div>
+			<div className="temperature">{feelslike}<span className="unit">{unit}</span></div>
+			</div>
 		);
 	}
 
