ad5e443df4ff31fb13f79ce1264be95192da9d5b
msoderquist <msoderquist@mastercontrol.com>
2025-06-12 17:38:33 -0600

    Add Zulu time to FlightConditions component

--- src/FlightConditions.js
+++ src/FlightConditions.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
 import './dashboard.css';
+import {getDowName, pad} from "./util";
 
 export default class FlightConditions extends Component {
 
+	state = {
+		yy: '',
+		mo: '',
+		dd: '',
+		hh: '',
+		mm: '',
+		ss: ''
+	};
+
+	componentDidMount() {
+		setTimeout(this.updateTime, 0);
+		this.refreshTimer = setInterval(this.updateTime, 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.refreshTimer);
+	}
+
+	updateTime = () => {
+		let timestamp = new Date();
+		let yy = timestamp.getUTCFullYear();
+		let mo = timestamp.getUTCMonth() + 1;
+		let dd = timestamp.getUTCDate();
+		let hh = timestamp.getUTCHours();
+		let mm = timestamp.getUTCMinutes();
+		let ss = timestamp.getUTCSeconds();
+		let day = timestamp.getUTCDay();
+		let dw = getDowName( day );
+
+		this.setState({yy: yy, mo: mo, dd: dd, hh: hh, mm: mm, ss: ss, dw: dw});
+	};
+
 	render() {
-		const isImperial = this.props.station.unitSystem === 'IMPERIAL';
 		let summary = (this.props.station.flightCondition && this.props.station.flightCondition.summary) || '';
 		let reasons = (this.props.station.flightCondition && this.props.station.flightCondition.reasons) || [];
 		return (
@@ -16,6 +48,7 @@ export default class FlightConditions extends Component {
 						<span key={index}>{index > 0 ? ' ' : ''}{reason}</span>
 					))}
 				</div>
+				<div className='time'>{pad(this.state.hh,2)}{pad( this.state.mm, 2)} Z</div>
 			</div>
 		);
 	}
